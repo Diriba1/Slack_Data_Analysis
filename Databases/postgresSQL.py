11fdc18a78fdb7facd71a23d2d43d98a1453278b
Diriba <39425889+Diriba1@users.noreply.github.com>
2023-12-01 22:13:47 +0300

    final commit

--- Databases/postgresSQL.py
+++ Databases/postgresSQL.py
@@ -15,43 +15,7 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Create tables
-cur.execute(
-    CREATE TABLE IF NOT EXISTS messages (
-    message_id SERIAL PRIMARY KEY,
-    workspace_id VARCHAR(255),
-    channel_id VARCHAR(255),
-    user_id VARCHAR(255),
-    timestamp TIMESTAMP,
-    content TEXT
-);
-
-CREATE TABLE IF NOT EXISTS replies (
-    reply_id SERIAL PRIMARY KEY,
-    message_id INTEGER REFERENCES messages(message_id),
-    user_id VARCHAR(255),
-    timestamp TIMESTAMP,
-    content TEXT
-);
-
-CREATE TABLE IF NOT EXISTS reactions (
-    reaction_id SERIAL PRIMARY KEY,
-    message_id INTEGER REFERENCES messages(message_id),
-    user_id VARCHAR(255),
-    emoji VARCHAR(50)
-);
-
-CREATE TABLE IF NOT EXISTS sentiments (
-    sentiment_id SERIAL PRIMARY KEY,
-    message_id INTEGER REFERENCES messages(message_id),
-    sentiment_score FLOAT
-);
-
-CREATE TABLE IF NOT EXISTS topics (
-    topic_id SERIAL PRIMARY KEY,
-    message_id INTEGER REFERENCES messages(message_id),
-    topic_name VARCHAR(255)
-);
-)
+cur.execute(sql_file.sql)
 
 # Insert sample data 
 cur.execute('''
